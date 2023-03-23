f1f2cdba7731718a579b6e7161e0a089d5e1cb28
Dongxu Li <dongxu_li@outlook.com>
2023-03-23 06:41:53 +0000

    first working version of refine.

--- modeling_zerobooth.py
+++ modeling_zerobooth.py
@@ -24,7 +24,7 @@ from diffusers import (
 )
 from modeling_clip import CtxCLIPTextModel
 import ptp_utils
-from ptp_utils import AttentionStore, P2PCrossAttnProcessor
+from ptp_utils import AttentionStore, P2PCrossAttnProcessor, AttentionRefine, LocalBlend
 
 
 class ProjLayer(nn.Module):
@@ -664,6 +664,149 @@ class ZeroBooth(nn.Module):
 
         return image
     
+    @torch.no_grad()
+    def generate_p2p_refine(
+        self,
+        samples,
+        controller,
+        guidance_scale=7.5,
+        height=512,
+        width=512,
+        init_latent=None,
+        seed=42,
+        num_inference_steps=250,
+        eta=1,
+        neg_prompt="",
+    ):
+        assert isinstance(controller, AttentionRefine)
+
+        self.register_attention_control(controller)
+
+        input_image = samples["input_images"]  # reference image
+        text_input = samples["class_names"]  # category
+        prompt = samples["prompt"]  # prompt for stable diffusion
+        batch_size = len(prompt)
+
+        scheduler = self.eval_noise_scheduler
+        # timesteps = self.scheduler.timesteps
+
+        # 1. extract BLIP query features and proj to text space -> (bs, 32, 768)
+        query_embeds = self.blip(image=input_image, text=text_input)
+        query_embeds = query_embeds[:, : self.num_query_token, :]
+        query_embeds = self.proj_layer(query_embeds)
+
+        # 2. embeddings for prompt, with query_embeds as context
+        # tokenized_prompt = self.tokenize_text(prompt).to(self.device)
+        tokenized_prompt_nosubj = self.tokenize_text(
+            prompt[:1], with_query=False).to(self.device)
+        tokenized_prompt = self.tokenize_text(
+            prompt[1:], with_query=True).to(self.device)
+
+        # FIXME assume the first sample in the subject contains no subject
+        text_embeddings_nosubj = self.text_encoder(
+            input_ids=tokenized_prompt_nosubj.input_ids,
+        )[0]
+        text_embeddings_subj = self.text_encoder(
+            input_ids=tokenized_prompt.input_ids,
+            ctx_embeddings=torch.cat([query_embeds] * (len(prompt) - 1), dim=0),
+            ctx_begin_pos=samples["ctx_begin_pos"] * (len(prompt) - 1),
+        )[0]
+
+        text_embeddings = torch.cat([text_embeddings_nosubj, text_embeddings_subj], dim=0)
+
+        # 3. unconditional embedding
+        do_classifier_free_guidance = guidance_scale > 1.0
+
+        if do_classifier_free_guidance:
+            max_length = self.text_encoder.text_model.config.max_position_embeddings 
+
+            uncond_input = self.tokenizer(
+                [neg_prompt],
+                padding="max_length",
+                max_length=max_length,
+                return_tensors="pt",
+            )
+
+            # FIXME use context embedding for uncond_input or not?
+            uncond_embeddings = self.text_encoder(
+                input_ids=uncond_input.input_ids.to(self.device),
+                ctx_embeddings=None,
+            )[0]
+            # repeat the uncond embedding to match the number of prompts
+            uncond_embeddings = uncond_embeddings.expand(len(prompt), -1, -1)
+
+            # For classifier free guidance, we need to do two forward passes.
+            # Here we concatenate the unconditional and text embeddings into a single batch
+            # to avoid doing two forward passes
+            text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+
+        if seed is not None:
+            generator = torch.Generator(device="cpu")
+            generator = generator.manual_seed(seed)
+
+        latents = self._init_latent(init_latent, height, width, generator, batch_size)
+
+        # set timesteps
+        accepts_offset = "offset" in set(
+            inspect.signature(scheduler.set_timesteps).parameters.keys()
+        )
+        extra_set_kwargs = {}
+        if accepts_offset:
+            extra_set_kwargs["offset"] = 1
+
+        scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+
+        # if we use LMSDiscreteScheduler, let's make sure latents are mulitplied by sigmas
+        if isinstance(scheduler, LMSDiscreteScheduler):
+            latents = latents * scheduler.sigmas[0]
+
+        # prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
+        # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
+        # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
+        # and should be between [0, 1]
+        accepts_eta = "eta" in set(inspect.signature(scheduler.step).parameters.keys())
+        extra_step_kwargs = {}
+        if accepts_eta:
+            extra_step_kwargs["eta"] = eta
+
+        iterator = tqdm.tqdm(scheduler.timesteps)
+
+        for i, t in enumerate(iterator):
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = (
+                torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+            )
+
+            noise_pred = self.unet(
+                latent_model_input, t, encoder_hidden_states=text_embeddings
+            )["sample"]
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (
+                    noise_pred_text - noise_pred_uncond
+                )
+
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = scheduler.step(noise_pred, t, latents, **extra_step_kwargs)[
+                "prev_sample"
+            ]
+
+            if controller is not None:
+                latents = controller.step_callback(latents)
+
+        # scale and decode the image latents with vae
+        latents = 1 / 0.18215 * latents
+        image = self.vae.decode(latents).sample
+
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).numpy()
+
+        image = numpy_to_pil(image)
+
+        return image
+
     def register_attention_control(self, controller):
         attn_procs = {}
         cross_att_count = 0
@@ -742,13 +885,17 @@ class ZeroBooth(nn.Module):
     #         images.append(image)
     #     ptp_utils.view_images(np.concatenate(images, axis=1))
 
-    def tokenize_text(self, text_input):
+    def tokenize_text(self, text_input, with_query=True):
+        max_len = self.text_encoder.text_model.config.max_position_embeddings
+        if with_query:
+            max_len -= self.num_query_token
+
         tokenized_text = self.tokenizer(
             text_input,
             padding="max_length",
             truncation=True,
             # max_length=self.text_encoder,
-            max_length=self.text_encoder.text_model.config.max_position_embeddings - self.num_query_token,
+            max_length=max_len,
             return_tensors="pt",
         )
 
