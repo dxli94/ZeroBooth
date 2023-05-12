e59d35d7b9c0c148c67524886c1b76b578453954
Dongxu Li <dongxu_li@outlook.com>
2023-05-12 14:55:55 +0000

    clip scores.

--- predict_benchmark.py
+++ predict_benchmark.py
@@ -8,6 +8,8 @@ import torch.backends.cudnn as cudnn
 from lavis.processors.blip_processors import BlipCaptionProcessor
 import numpy as np
 import random
+from PIL import Image
+import clip
 
 debug = False
 seed = 1234
@@ -152,6 +154,45 @@ def load_checkpoint(checkpoint_path):
         model.load_checkpoint(checkpoint_path)
         print("Finished loading checkpoint.")
 
+
+def load_clip():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load('ViT-B/32', device)
+
+    return model, preprocess
+
+
+def get_clip_image_features_dir(dirpath, clip_model, preprocess):
+    filenames = sorted(os.listdir(dirpath))
+    # keep only images
+    filenames = [f for f in filenames if f.endswith(".png") or f.endswith(".jpg") or f.endswith(".jpeg")]
+    # concatenate with dirpath
+    filenames = [os.path.join(dirpath, f) for f in filenames]
+
+    features = []
+
+    for filename in filenames:
+        image = Image.open(filename)
+        image_input = preprocess(image).unsqueeze(0).to(device)
+        with torch.no_grad():
+            image_features = clip_model.encode_image(image_input).float()
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+
+        features.append(image_features)
+    
+    return torch.cat(features, dim=0)
+
+
+def get_clip_text_features(text, clip_model):
+    text = clip.tokenize([text]).to(device)
+
+    with torch.no_grad():
+        text_features = clip_model.encode_text(text).float()
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+    
+    return text_features
+
+
 def generate_images(
         class_token,
         prompt,
@@ -188,6 +229,43 @@ def generate_images(
 
     return output[0]
 
+def compute_clip_score(clip_model, preprocess, image, ref_features):
+    image_input = preprocess(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        image_features = clip_model.encode_image(image_input).float()
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+    
+    sims = image_features @ ref_features.T
+
+    return sims.mean().item()
+
+
+def get_scores_by_class_tokens(total_records):
+    # records = [{"model_id": "sth", "clip_i_score": 0.3, "clip_t_score": 0.3}]
+    records = total_records
+    scores_by_class_tokens = {}
+
+    for record in records:
+        class_token = record["model_id"]
+        clip_i_score = record["clip_i_score"]
+        clip_t_score = record["clip_t_score"]
+
+        if class_token not in scores_by_class_tokens:
+            scores_by_class_tokens[class_token] = {
+                "clip_i_scores": [],
+                "clip_t_scores": [],
+            }
+        
+        scores_by_class_tokens[class_token]["clip_i_scores"].append(clip_i_score)
+        scores_by_class_tokens[class_token]["clip_t_scores"].append(clip_t_score)
+
+    # take mean
+    for class_token in scores_by_class_tokens:
+        scores_by_class_tokens[class_token]["clip_i_scores"] = sum(scores_by_class_tokens[class_token]["clip_i_scores"]) / len(scores_by_class_tokens[class_token]["clip_i_scores"])
+        scores_by_class_tokens[class_token]["clip_t_scores"] = sum(scores_by_class_tokens[class_token]["clip_t_scores"]) / len(scores_by_class_tokens[class_token]["clip_t_scores"])
+    
+    return scores_by_class_tokens
+
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -206,12 +284,16 @@ rank = get_rank()
 num_images = 4
 
 checkpoint_base = "/export/home/workspace/dreambooth/diffusers/output/benchmark/checkpoints"
+
+image_in_base = "/export/home/workspace/dreambooth/diffusers/official_benchmark/dreambooth/dataset"
 image_out_base = "/export/home/workspace/dreambooth/diffusers/output/benchmark/images"
+if debug:
+    image_out_base = "/export/home/workspace/dreambooth/diffusers/output/debug/images"
 
 # csv file looks like this:
 # model_id, path_to_best_checkpoint, class_token, is_live_object (bool)
 best_ckpt_file = "/export/home/workspace/dreambooth/diffusers/evaluations/best-checkpoints.csv"
-df = pd.read_csv(best_ckpt_file)
+df = pd.read_csv(best_ckpt_file, header=None)
 df = df if not debug else df[:4]
 
 num_models = len(df)
@@ -227,14 +309,18 @@ model = create_model()
 df_rank = df[start_id:end_id+1]
 print("Rank {} will process {} to {}.".format(rank, start_id, end_id))
 
+clip_model, clip_preprocess = load_clip()
+
 # iterate over each model
 for index, row in df_rank.iterrows():
     model_id, best_ckpt, class_token, is_live_object = row
-
     load_checkpoint(best_ckpt)
 
     image_outdir = os.path.join(image_out_base, now, model_id)
     os.makedirs(image_outdir, exist_ok=True)
+
+    image_indir = os.path.join(image_in_base, model_id)
+    ref_image_features = get_clip_image_features_dir(image_indir, clip_model, clip_preprocess)
 
     if is_live_object:
         prompts = live_prompts
@@ -244,6 +330,9 @@ for index, row in df_rank.iterrows():
     prompts = prompts if not debug else prompts[:2]
 
     for prompt in prompts:
+        prompt_with_class = f"a {class_token} {prompt}"
+        ref_text_features = get_clip_text_features(prompt_with_class, clip_model)
+
         for i in range(num_images):
             this_seed = seed + i
             image = generate_images(class_token, prompt, this_seed)
@@ -252,14 +341,19 @@ for index, row in df_rank.iterrows():
             image_path = os.path.join(image_outdir, image_name)
             image.save(image_path)
 
+            clip_i_scores = compute_clip_score(clip_model, clip_preprocess, image, ref_image_features)
+            clip_t_scores = compute_clip_score(clip_model, clip_preprocess, image, ref_text_features)
+            print(clip_i_scores, clip_t_scores)
+
             record = {
                 "image_name": image_name,
                 "prompt": prompt,
                 "model_id": model_id,
                 "seed": this_seed,
+                "clip_i_score": clip_i_scores,
+                "clip_t_score": clip_t_scores,
             }
             records.append(record)
-            # image.save("dog-bucket.png")
 
 import json
 
@@ -279,4 +373,25 @@ if rank == 0:
     with open(os.path.join(image_out_base, now, "records.json"), "w") as f:
         json.dump(all_records, f)
 
-# python -m torch.distributed.launch --nproc_per_node=2 predict_benchmark.py --world_size 4 --distributed True
+    # compute average scores
+    total_clip_i_score = 0
+    total_clip_t_score = 0
+
+    for record in all_records:
+        total_clip_i_score += record["clip_i_score"]
+        total_clip_t_score += record["clip_t_score"]
+
+    avg_clip_i_score = total_clip_i_score / len(all_records)
+    avg_clip_t_score = total_clip_t_score / len(all_records)
+
+    metrics = {
+        "avg_clip_i_score": avg_clip_i_score,
+        "avg_clip_t_score": avg_clip_t_score,
+    }
+
+    scores_by_class_tokens = get_scores_by_class_tokens(all_records)
+    metrics["scores_by_class_tokens"] = scores_by_class_tokens
+
+    with open(os.path.join(image_out_base, now, "metrics.json"), "w") as f:
+        json.dump(metrics, f, indent=4, sort_keys=True)
+
